c3df564d22003117b4f1560429b172fa4a8c22cb
Willow Dimmer <dimmerw@vlan4-dhcp-4-223.aadl.org>
2025-06-19 09:54:38 -0400

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+ let date: string = new Date().toLocaleDateString(
+    'en-us',
+    {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    }
+  );
+</script>
+
+<main>
+  <img src="https://media.aadl.com/media/production_resources/endCard/aadl-logo.png" style="width: 17vw;"/>
+  <h1><strong>AADL PRODUCTIONS</strong></h1>
+  <p>Produced on {date} <br/> by the Ann Arbor District Library</p>
+  <img src="https://media.aadl.com/media/production_resources/endCard/by-nc-sa.png" style="width: 17vw;"/>
+</main>
+
+<style>
+
+</style>
