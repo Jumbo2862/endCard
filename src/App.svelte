6ad8e964b32ff6273ef6364952cdeff3d970003f
Willow Dimmer <dimmerw@vlan4-dhcp-4-223.aadl.org>
2025-07-05 08:43:56 -0400

    refactor: Use local images rather than hosted

--- src/App.svelte
+++ src/App.svelte
@@ -10,10 +10,10 @@
 </script>
 
 <main>
-  <img src="https://media.aadl.com/media/production_resources/endCard/aadl-logo.png" style="width: 17vw;"/>
+  <img src="/aadl-logo.png" style="width: 17vw;"/>
   <h1><strong>AADL PRODUCTIONS</strong></h1>
   <p>Produced on {date} <br/> by the Ann Arbor District Library</p>
-  <img src="https://media.aadl.com/media/production_resources/endCard/by-nc-sa.png" style="width: 17vw;"/>
+  <img src="/by-nc-sa.png" style="width: 17vw;"/>
 </main>
 
 <style>
